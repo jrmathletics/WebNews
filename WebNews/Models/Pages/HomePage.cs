a85b6dcc59925a400e9cb3e9ef66c84f34204c6e
unknown <mathias.olsen@MWNOOSLWC8LT179.makingwaves.no>
2015-08-17 14:26:48 +0200

    Installed Bootstrap, layout-file added

---
+++ WebNews/Models/Pages/HomePage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace WebNews.Models.Pages
+{
+    [ContentType(DisplayName = "HomePage", GUID = "538873f2-bcd1-4709-b066-f73f3fb669d3", Description = "")]
+    public class HomePage : PageData
+    {
+        
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         
+    }
+}
