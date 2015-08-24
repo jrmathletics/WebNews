15a182dcd87ee8ab3006c445f04456dd53e2177b
unknown <mathias.olsen@MWNOOSLWC8LT179.makingwaves.no>
2015-08-24 13:05:27 +0200

    PersonPage complete-ish. Text still needs resizing.

--- WebNews/Models/Pages/FolderPage.cs
+++ WebNews/Models/Pages/FolderPage.cs
@@ -10,9 +10,9 @@ using EPiServer.SpecializedProperties;
 namespace WebNews.Models.Pages
 {
     [ContentType(DisplayName = "FolderPage", GUID = "c3886e90-7404-4c62-89a0-70d339c20a70", Description = "",
-        AvailableInEditMode = false)]
+        AvailableInEditMode = true)]
 
-    public class FolderPage : BaseEditorialPage, IContentData
+    public class FolderPage : PageData, IContentData
     {
         public override void SetDefaultValues(ContentType contentType)
         {
---
+++ WebNews/Models/Pages/PersonPage.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using EPiServer.Web;
+
+namespace WebNews.Models.Pages
+{
+    [ContentType(DisplayName = "PersonPage", GUID = "fa18659a-c502-43da-96bc-21561af2f8d4", Description = "")]
+    public class PersonPage : BaseEditorialPage
+    {
+
+        [CultureSpecific]
+        [Required(ErrorMessage = "Fornavn må være fylt ut!")]
+        [Display(
+            Name = "Fornavn",
+            GroupName = SystemTabNames.Content,
+            Order = 3)]
+        public virtual string FirstName { get; set; }
+
+        [CultureSpecific]
+        [Required(ErrorMessage = "Etternavn må være fylt ut!")]
+        [Display(
+            Name = "Etternavn",
+            GroupName = SystemTabNames.Content,
+            Order = 4)]
+        public virtual string LastName { get; set; }
+
+        [CultureSpecific]
+        [Required(ErrorMessage = "Adresse må være fylt ut!")]
+        [Display(
+            Name = "Adresse",
+            GroupName = SystemTabNames.Content,
+            Order = 5)]
+        public virtual string Street { get; set; }
+
+        [CultureSpecific]
+        [Required(ErrorMessage = "Postnummer må være fylt ut!")]
+        [Display(
+            Name = "Postnummer(må være 4 siffer)",
+            GroupName = SystemTabNames.Content,
+            Order = 6)]
+        [RegularExpression("\\d{4}",
+        ErrorMessage = "Ugyldig format på postnummer!")]
+        public virtual string PostalCode { get; set; }
+
+        [CultureSpecific]
+        [Required(ErrorMessage = "By må være fylt ut!")]
+        [Display(
+            Name = "By",
+            GroupName = SystemTabNames.Content,
+            Order = 7)]
+        public virtual string City { get; set; }
+
+
+        [CultureSpecific]
+        [Required (ErrorMessage = "E-post må være fylt ut!")]
+        [Display(
+            Name = "E-postadresse",
+            GroupName = SystemTabNames.Content,
+            Order = 7)]
+        [EmailAddress(ErrorMessage = "Ugyldig e-postadresse")]
+        [DataType(DataType.EmailAddress)]
+        public virtual string EmailAddress { get; set; }
+
+        [CultureSpecific]
+        [Required(ErrorMessage = "Telefonnummer må være fylt ut!")]
+        [Display(
+            Name = "Telefonnummer",
+            GroupName = SystemTabNames.Content,
+            Order = 7)]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression("^[0-9]{8}$")]
+
+        public virtual string PhoneNumber { get; set; }
+
+        [CultureSpecific]
+        [Required(ErrorMessage = "Du må legge ved bilde!")]
+        [Display(
+            Name = "Personbilde",
+            GroupName = SystemTabNames.Content,
+            Order = 7)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference PersonImage { get; set; }
+
+    }
+}
