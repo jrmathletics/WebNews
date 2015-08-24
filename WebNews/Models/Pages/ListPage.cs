62f3b38c4f51cdb611a2859013b32ff062c3bd61
unknown <mathias.olsen@MWNOOSLWC8LT179.makingwaves.no>
2015-08-24 14:15:44 +0200

    First "attempts" at ListPage.

---
+++ WebNews/Models/Pages/ListPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace WebNews.Models.Pages
+{
+    [ContentType(DisplayName = "ListPage", GUID = "67aee80d-54c3-40a8-8c2c-79a49fb1bfd2", Description = "")]
+    public class ListPage : BaseEditorialPage
+    {
+        
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         
+    }
+}
