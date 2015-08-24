1d4b7669b8e9aaf7143a77aa4343a9889340ac56
unknown <mathias.olsen@MWNOOSLWC8LT179.makingwaves.no>
2015-08-24 10:58:54 +0200

    Started on ArticlePage. Fiddling with HTML/CSS to make it look right.

---
+++ WebNews/Models/Pages/ArticlePage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using EPiServer.Web;
+
+namespace WebNews.Models.Pages
+{
+    [ContentType(DisplayName = "ArticlePage", GUID = "ba296511-df1d-400a-b2ac-9923d06e118e", Description = "")]
+    public class ArticlePage : BaseEditorialPage
+    {
+        [UIHint(UIHint.LongString)]
+        [CultureSpecific]
+        [Display(
+                    Name = "Artikkeltekst",
+                    GroupName = SystemTabNames.Content,
+                    Order = 3)]
+        public virtual XhtmlString BodyText { get; set; }
+
+    }
+}
---
+++ WebNews/Models/Pages/BaseEditorialPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using EPiServer.Web;
+
+namespace WebNews.Models.Pages
+{
+    [ContentType(DisplayName = "BaseEditorialPage", GUID = "b8512201-40cb-4cb9-b9ca-0fff38ee122e", Description = "")]
+    public abstract class BaseEditorialPage : PageData
+    {
+
+        [CultureSpecific]
+        [Display(
+        Name = "Overskrift",
+        GroupName = SystemTabNames.Content,
+        Order = 1)]
+        public virtual string Header { get; set; }
+
+        [UIHint(UIHint.LongString)]
+        [CultureSpecific]
+        [Display(
+                    Name = "Ingress",
+                    GroupName = SystemTabNames.Content,
+                    Order = 2)]
+        public virtual XhtmlString IntroText { get; set; }
+
+    }
+}
